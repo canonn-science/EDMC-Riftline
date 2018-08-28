456f379af051f23f5dfd62955b895b8e811b397c
NoFoolLikeOne <31373535+NoFoolLikeOne@users.noreply.github.com>
2018-08-28 18:39:12 +0100

    Get Coordinates First
    Changed to get the coordinates first and then calculate distance using maths that I understand!
    
    Precursor to displaying the closest system

--- load.py
+++ load.py
@@ -3,41 +3,55 @@ A Skeleton EDMC Plugin
 """
 import locale
 import sys
-
+import math
 
 import Tkinter
 
 this = sys.modules[__name__]
 locale.setlocale(locale.LC_ALL, '')
 
+RIEDQUAT=(68.84375, 48.75, 69.75)
+REORTE=(75.75, 48.75, 75.15625)
+
+
 def dot(a, b):
 	return sum([a[i]*b[i] for i in range(3)])
 
-def norm(a):
-	return sum([a[i]**2 for i in range(3)])**0.5
+
+def getNearest(x,y,z):
+        # gets the coordinates of the nearest point on a line extending through Riedquat and Reorte
+        r = RIEDQUAT
+        q = REORTE
+        p = (x, y, z)
+
+        # avoid divide by zero if the x,y,z is one of the end points
+        if (str(r) == str(p)):
+                return r
+        if (str(r) == str(q)):
+                return r
+        # https://math.stackexchange.com/questions/1521128/given-a-line-and-a-point-in-3d-how-to-find-the-closest-point-on-the-line
+        # find the magnitude along the line at the closestpoint
+        # t=(r-q).(q-p)/(r-q).(r-q)
+        # that can be used in the formula g=q+t^(r.q) to derive the
+        # point g on the line that is closest to point p.
+
+        # define variables for r-q and q-p
+        rq = tuple([r[i]-q[i] for i in range(3)])
+        qp = tuple([p[i]-q[i] for i in range(3)])
+
+        # this gets the distance along the line of the nearest point
+        t=dot(rq,qp)/dot(rq,rq)
+        # This calculates the coordinates of the nearest point
+        g=tuple([q[i]+(t*r[i]-t*q[i]) for i in range(3)])
+
+        return g
 
 def getDistance(x,y,z):
-	#The Rift Line is a line that passes through Riedquat and Reorte
-	#So hard coding the coords here
-	p = (68.84375, 48.75, 69.75)
-	q = (75.75, 48.75, 75.15625)
-	r = (x, y, z)
-    
-	def t(p, q, r):
-		a = tuple([r[i]-q[i] for i in range(3)])
-		b = tuple([p[i]-q[i] for i in range(3)])
-		return dot(a, b)/dot(b, b)
-    
-	def d(p, q, r):
-		k = t(p, q, r)
-		a = tuple([k*(p[i]-q[i]) for i in range(3)])
-		b = tuple([a[i]+q[i] for i in range(3)])
-		c = tuple([b[i]-r[i] for i in range(3)])
-		return norm(c)
-    
-	return d(p, q, r)
+        # gets the distance to a line extending through Riedquat and Reorte
+        p=(x,y,z)
+        g=getNearest(x,y,z)
 
-
+        return math.sqrt(sum(tuple([math.pow(p[i]-g[i],2)  for i in range(3)])))
 
 
 def plugin_start():
